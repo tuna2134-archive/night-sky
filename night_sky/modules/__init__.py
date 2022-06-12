668b1581edbe3078d1593f5fdc3307a975b69857
mc_fdc <Masato.04.11.2007@gmail.com>
2022-06-12 12:05:09 +0900

    Update __init__.py

--- night_sky/modules/__init__.py
+++ night_sky/modules/__init__.py
@@ -1,6 +1,6 @@
-from .shell import ShellCog
+from .manager import Manager
 from discord.ext import commands
 
 
 async def setup(bot: commands.Bot):
-    await bot.add_cog(ShellCog(bot))
+    await bot.add_cog(Manager(bot))
